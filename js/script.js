8705bbee36218209a99f35180e433547c0f8d6f8
Winters2104 <adrianhartanto153@gmail.com>
2024-06-04 11:52:49 +0700

    update masking

--- js/script.js
+++ js/script.js
@@ -15,9 +15,17 @@ document.querySelector('#search-btn').onclick = (e) => {
     e.preventDefault();
 }
 
+const shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart-btn').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+}
+
 // klik dluar element
 const hamburger = document.querySelector('#hamburger-menu');
 const searchButton = document.querySelector('#search-btn');
+const shopping = document.querySelector('#shopping-cart-btn');
 
 document.addEventListener('click', function(e){
     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
@@ -26,5 +34,9 @@ document.addEventListener('click', function(e){
 
     if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
       searchForm.classList.remove('active');
+    }
+    
+    if (!shopping.contains(e.target) && !shoppingCart.contains(e.target)) {
+      shoppingCart.classList.remove('active');
     }
 });
